2888aa910b8a56f6907afd53621b575df55f751d
MARIA52SWEETBONANZA1000 <mariaheroin@mail.ru>
2025-09-18 20:21:02 +0300

    с++.cpp

--- с++.cpp
+++ с++.cpp
@@ -1,1 +1,25 @@
-#include <iostream>#include <stack>#include <array>using namespace std;int main() {    // Массив    int numbers[] = {10, 20, 30, 40, 50};    cout << "Массив: ";    for (int value : numbers) cout << value << " ";    cout << endl;        // Стек    stack<int> dataStack;    dataStack.push(100);    dataStack.push(200);    dataStack.push(300);        cout << "Текущий верхний элемент стека: " << dataStack.top() << endl;    cout << "Извлекаем элемент: " << dataStack.top() << endl;    dataStack.pop();    cout << "Новый верхний элемент после извлечения: " << dataStack.top() << endl;        return 0;}
+#include <iostream>
+#include <stack>
+#include <array>
+using namespace std;
+
+int main() {
+    // РњР°СЃСЃРёРІ
+    int numbers[] = {10, 20, 30, 40, 50};
+    cout << "РњР°СЃСЃРёРІ: ";
+    for (int value : numbers) cout << value << " ";
+    cout << endl;
+    
+    // РЎС‚РµРє
+    stack<int> dataStack;
+    dataStack.push(100);
+    dataStack.push(200);
+    dataStack.push(300);
+    
+    cout << "РўРµРєСѓС‰РёР№ РІРµСЂС…РЅРёР№ СЌР»РµРјРµРЅС‚ СЃС‚РµРєР°: " << dataStack.top() << endl;
+    cout << "РР·РІР»РµРєР°РµРј СЌР»РµРјРµРЅС‚: " << dataStack.top() << endl;
+    dataStack.pop();
+    cout << "РќРѕРІС‹Р№ РІРµСЂС…РЅРёР№ СЌР»РµРјРµРЅС‚ РїРѕСЃР»Рµ РёР·РІР»РµС‡РµРЅРёСЏ: " << dataStack.top() << endl;
+    
+    return 0;
+}
